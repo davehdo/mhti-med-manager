caff03f6d31167992a5401e5d0bafcbe9a005944
David Do <davehdo@gmail.com>
2016-08-10 11:41:15 -0700

    medline plus entries save

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -2,8 +2,19 @@ class Medication
   include Mongoid::Document
   field :name, type: String
   field :dosage, type: String
+  field :education, type: Array 
   field :description, type: String
   field :take_am, type: Boolean
   field :take_noon, type: Boolean
   field :take_pm, type: Boolean
+  
+  before_save :fetch_medline
+  
+  def fetch_medline
+    begin
+      self.education ||= MedlinePlus2.find_drug_by_name( self.name || “atorvastatin” )
+    rescue
+      self.education ||= []
+    end
+  end
 end
--- app/views/medications/_medication.json.jbuilder
+++ app/views/medications/_medication.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! medication, :name, :dosage, :description, :take_am, :take_noon, :take_pm
+json.extract! medication, :name, :dosage, :description, :take_am, :take_noon, :take_pm, :education
 json.id medication.to_param
